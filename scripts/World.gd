364a7c2b21822fb27e567673fb0fae3162051e95
AndreMicheletti <andre.micheletti@zenvia.com>
2020-09-30 16:10:26 -0300

    randomness

--- scripts/World.gd
+++ scripts/World.gd
@@ -117,6 +117,7 @@ func _unhandled_input(event):
 			# print("poiting at hex ", hex.get_axial_coords())
 			
 			$Highlight.position = get_grid().get_hex_center(hex)
+			print("hex x/y ", $Highlight.position)
 			# var path = get_grid().find_path(Vector2(0, 0), hex)
 			# print(path.size())
 			if is_outside_world(hex.get_axial_coords()):
