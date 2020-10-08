da6a59489fe9fffc96264d7b5d125fded3019eeb
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2020-10-08 14:16:22 -0300

    exporting for android

---
+++ global.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var classes = [
+	preload("res://resources/classes/samurai.tres"),
+	preload("res://resources/classes/elite.tres"),
+	preload("res://resources/classes/monk.tres"),
+]
+
+var selectedPlayerClass = classes[0]
+var selectedEnemyClass = classes[0]
+
+func set_player_class(idx):
+	selectedPlayerClass = classes[idx]
+
+func set_enemy_class(idx):
+	selectedEnemyClass = classes[idx]
